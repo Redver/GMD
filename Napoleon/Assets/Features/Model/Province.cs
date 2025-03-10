afb1f93aa0bd865b90d2f914778da39d3981725d
Redver <116087243+Redver@users.noreply.github.com>
2025-03-10 11:51:58 +0100

    gmd

--- Napoleon/Assets/Features/Model/Province.cs
+++ Napoleon/Assets/Features/Model/Province.cs
@@ -1,12 +1,38 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Province : MonoBehaviour
 {
-    private List<Province> NeigbourProvinces = new List<Province>();
-    void Start()
+    [SerializeField] private List<Province> NeigbourProvinces = new List<Province>();
+
+    private void Awake()
     {
         
+    }
+    
+    void Start()
+    {
+        FindNeighbors();
+    }
+
+    void FindNeighbors()
+    {
+        BoxCollider col = GetComponent<BoxCollider>();
+        if (col == null) return;
+
+        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale, Quaternion.identity);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            Debug.Log("Hit from "+ gameObject.name+" : " + hitColliders[i].name + i);
+            if (hitColliders[i].name != gameObject.name)
+            {
+                NeigbourProvinces.Add(hitColliders[i].GetComponent<Province>());
+            }
+            i++;
+        }
+        //if name = gameobject name ignore, else add to neigbours list
     }
 
     void Update()
